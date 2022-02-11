fcfb7b444294b4ed86c2529ee02476a1e6b46670
paduh <perfect.aduh@gmail.com>
2022-02-11 01:32:53 +0100

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Perfect Aduh on 11/02/2022.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUrl: URL
+}
